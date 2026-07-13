ac6346083c6cbe98c31af9c4e1fac39f2c17b71b
roshini <khroshini00@gmail.com>
2026-07-13 12:33:40 +0530

    roshini

--- resume_builder/admin.py
+++ resume_builder/admin.py
@@ -1,3 +1,28 @@
-from django.contrib import admin
+
 
 # Register your models here.
+from django.contrib import admin
+
+from .models import (
+    User,
+    Profile,
+    Resume,
+    Education,
+    Experience,
+    Skill,
+    Project,
+    CompanyProfile,
+    Job,
+    Application,
+)
+
+admin.site.register(User)
+admin.site.register(Profile)
+admin.site.register(Resume)
+admin.site.register(Education)
+admin.site.register(Experience)
+admin.site.register(Skill)
+admin.site.register(Project)
+admin.site.register(CompanyProfile)
+admin.site.register(Job)
+admin.site.register(Application)
